afd0a949890eca820ef281f97a6684044cab913b
Myles O'Connor <mocon@guidance.com>
2013-05-17 16:16:06 -0700

    Temporary fix to jQuery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,66 @@
 $(document).ready(function() {
-	
-	$('.petal').mouseenter(function () {
+	$('.petal .a-1').hover(function () {
 		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
 		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
-	});	
+	});
+	$('.petal .a-1').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-2').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-2').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-3').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-3').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-4').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-4').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-5').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-5').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-6').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-6').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-7').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-7').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
+	$('.petal .a-8').hover(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
+	});
+	$('.petal .a-8').mouseleave(function () {
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
+		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
+	});
 });
