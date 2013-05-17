4c6666c48f5e42d0216787a1a624f615ba8e9bad
Myles O'Connor <mocon@guidance.com>
2013-05-17 16:37:35 -0700

    Began jQuery keyboard input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// petal hover functions
 	$('.petal .a-1').hover(function () {
 		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('down');
 		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('up');
@@ -63,4 +64,27 @@ $(document).ready(function() {
 		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').removeClass('up');
 		$(this).children('.button-blue, .button-yellow, .button-red, .button-green').addClass('down');
 	});
+	
+	// key press functions
+	//$(document).keypress("c",function(e) {
+	  //if(e.ctrlKey)
+	    //alert("Ctrl+C was pressed!!");
+	//});
+	$(document.documentElement).keyup(function (event) {
+	  // handle cursor keys
+	  if (event.keyCode == 37) {
+	    // go left
+	    alert('Left.');
+	  } else if (event.keyCode == 39) {
+	    // go right
+	    alert('Right.');
+	  } else if (event.keyCode == 40) {
+	    // go right
+	    alert('Down.');
+	  } else if (event.keyCode == 38) {
+	    // go right
+	    alert('Up.');
+	  }
+	});
+	
 });
