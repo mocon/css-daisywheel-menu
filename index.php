841db90d6815578a3b82c8b9772583b087d245dc
Myles O'Connor <mocon@guidance.com>
2013-05-17 13:24:40 -0700

    CSS structure complete, without :hover effects

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>   
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<!--[if IE]><![endif]-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Myles</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="">
+	<!--<meta name="viewport" content="width=device-width; initial-scale=1.0">-->
+	<!-- !CSS -->
+	<link rel="stylesheet" href="css/myles.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+</head>
+<!-- !Body -->
+<body class="radial-center">
+	<div id="container">
+		<header>
+			
+		</header><!-- /header -->
+		
+		<section id="main">
+			
+			<div class="circle">
+				<div class="petal a-1">
+					<div class="petal-circle">
+						<div class="buttons a-1">
+							<div class="button-blue">
+								<div class="button-text">
+									A
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									B
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									C
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									D
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-1 -->
+				<div class="petal a-2">
+					<div class="petal-circle">
+						<div class="buttons a-2">
+							<div class="button-blue">
+								<div class="button-text">
+									E
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									F
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									G
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									H
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-2 -->
+				<div class="petal a-3">
+					<div class="petal-circle">
+						<div class="buttons a-3">
+							<div class="button-blue">
+								<div class="button-text">
+									I
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									J
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									K
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									L
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-3 -->
+				<div class="petal a-4">
+					<div class="petal-circle">
+						<div class="buttons a-4">
+							<div class="button-blue">
+								<div class="button-text">
+									M
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									N
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									O
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									P
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-4 -->
+				<div class="petal a-5">
+					<div class="petal-circle">
+						<div class="buttons a-5">
+							<div class="button-blue">
+								<div class="button-text">
+									Q
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									R
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									S
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									T
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-5 -->
+				<div class="petal a-6">
+					<div class="petal-circle">
+						<div class="buttons a-6">
+							<div class="button-blue">
+								<div class="button-text">
+									U
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									V
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									W
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									X
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-6 -->
+				<div class="petal a-7">
+					<div class="petal-circle">
+						<div class="buttons a-7">
+							<div class="button-blue">
+								<div class="button-text">
+									Y
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									Z
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									?
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									!
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-7 -->
+				<div class="petal a-8">
+					<div class="petal-circle">
+						<div class="buttons a-8">
+							<div class="button-blue">
+								<div class="button-text">
+									;
+								</div>
+							</div>
+							<div class="button-yellow">
+								<div class="button-text">
+									/
+								</div>
+							</div>
+							<div class="button-red">
+								<div class="button-text">
+									&
+								</div>
+							</div>
+							<div class="button-green">
+								<div class="button-text">
+									_
+								</div>
+							</div>
+						</div><!-- /.buttons -->
+					</div><!-- /.petal-circle -->
+				</div><!-- /.petal.a-8 -->				
+			</div>
+			
+		</section><!-- /main -->
+		
+		<footer>
+		
+		</footer><!-- /footer -->
+	</div><!--!/#container -->
+</body>
+</html>
