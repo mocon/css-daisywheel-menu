1ed56db001f6ed27c9e90b6947d417299301f7cd
Myles O'Connor <mocon@guidance.com>
2013-05-17 15:49:02 -0700

    jQuery in progress

--- index.php
+++ index.php
@@ -8,44 +8,43 @@
 	<meta charset="utf-8">
 	<!--[if IE]><![endif]-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Myles</title>
+	<title>Steam Daisywheel Selector</title>
 	<meta name="description" content="">
 	<meta name="keywords" content="" />
 	<meta name="author" content="">
 	<!--<meta name="viewport" content="width=device-width; initial-scale=1.0">-->
 	<!-- !CSS -->
-	<link rel="stylesheet" href="css/myles.css">
+	<link rel="stylesheet" href="css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
 </head>
 <!-- !Body -->
 <body class="radial-center">
 	<div id="container">
 		<header>
-			
 		</header><!-- /header -->
-		
 		<section id="main">
-			
 			<div class="circle">
 				<div class="petal a-1">
 					<div class="petal-circle">
 						<div class="buttons a-1">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									A
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									B
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									C
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									D
 								</div>
@@ -56,22 +55,22 @@
 				<div class="petal a-2">
 					<div class="petal-circle">
 						<div class="buttons a-2">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									E
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									F
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									G
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									H
 								</div>
@@ -82,22 +81,22 @@
 				<div class="petal a-3">
 					<div class="petal-circle">
 						<div class="buttons a-3">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									I
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									J
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									K
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									L
 								</div>
@@ -108,22 +107,22 @@
 				<div class="petal a-4">
 					<div class="petal-circle">
 						<div class="buttons a-4">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									M
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									N
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									O
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									P
 								</div>
@@ -134,22 +133,22 @@
 				<div class="petal a-5">
 					<div class="petal-circle">
 						<div class="buttons a-5">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									Q
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									R
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									S
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									T
 								</div>
@@ -160,22 +159,22 @@
 				<div class="petal a-6">
 					<div class="petal-circle">
 						<div class="buttons a-6">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									U
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									V
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									W
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									X
 								</div>
@@ -186,22 +185,22 @@
 				<div class="petal a-7">
 					<div class="petal-circle">
 						<div class="buttons a-7">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									Y
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									Z
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									?
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									!
 								</div>
@@ -212,22 +211,22 @@
 				<div class="petal a-8">
 					<div class="petal-circle">
 						<div class="buttons a-8">
-							<div class="button-blue">
+							<div class="button-blue down">
 								<div class="button-text">
 									;
 								</div>
 							</div>
-							<div class="button-yellow">
+							<div class="button-yellow down">
 								<div class="button-text">
 									/
 								</div>
 							</div>
-							<div class="button-red">
+							<div class="button-red down">
 								<div class="button-text">
 									&
 								</div>
 							</div>
-							<div class="button-green">
+							<div class="button-green down">
 								<div class="button-text">
 									_
 								</div>
@@ -236,11 +235,8 @@
 					</div><!-- /.petal-circle -->
 				</div><!-- /.petal.a-8 -->				
 			</div>
-			
 		</section><!-- /main -->
-		
 		<footer>
-		
 		</footer><!-- /footer -->
 	</div><!--!/#container -->
 </body>
